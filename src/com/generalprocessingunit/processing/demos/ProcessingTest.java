2c0ed636ffa6f3e4ef2777b88113aeb6e4713770
Paul M. Christian <paulmchristian@gmail.com>
2014-09-10 00:30:44 -0500

    make processing test windowed

--- src/com/generalprocessingunit/processing/demos/ProcessingTest.java
+++ src/com/generalprocessingunit/processing/demos/ProcessingTest.java
@@ -5,34 +5,30 @@ import processing.core.PApplet;
 public class ProcessingTest extends PApplet {
 
     public static void main(String[] args){
-        PApplet.main(new String[]{"--full-screen", "--display=3", ProcessingTest.class.getCanonicalName()});
+        PApplet.main(new String[]{/*"--full-screen", "--display=3",*/ ProcessingTest.class.getCanonicalName()});
         // if fullscreen works, --display=hmd.DisplayId
     }
 
-    @Override
-    public void init() {
-        // http://wiki.processing.org/w/Undecorated_frame
-//        frame.removeNotify();
-//        frame.setUndecorated(true);
-        super.init();
-    }
+//    @Override
+//    public void init() {
+//        // http://wiki.processing.org/w/Undecorated_frame
+////        frame.removeNotify();
+////        frame.setUndecorated(true);
+//        super.init();
+//    }
 
     @Override
     public void setup() {
 
-        size(1922, 1080, OPENGL);
-        super.setup();
+        size(1200, 800, OPENGL);
 //        frame.setLocation(0,1200);
-
-
-
-
 
     }
 
     @Override
     public final void draw() {
 
+        background(255, 0, 0);
         System.out.println(frameRate);
 
     }
